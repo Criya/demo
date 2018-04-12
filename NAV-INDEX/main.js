396e44332cb803260137e5e27d2a6f731e9bd2e4
ljh <criya713@163.com>
2018-04-12 22:13:38 +0800

    修正了键盘大写没反应的bug

--- NAV-INDEX/main.js
+++ NAV-INDEX/main.js
@@ -25,20 +25,16 @@ if(hashGeiInLocal)
 
 for (var i = 0; i < keys.length; i++) {
     rows = keys[i]  /*模拟键盘的一二三排*/
-    div1 = document.createElement('div')
-    div1.className = 'row'
+    div1 = tag('div', {className:'row'})
+
     for (var j = 0; j < rows.length; j++) {
         /*添加上的字母*/
-        kbd1 = document.createElement('kbd')
-        span1 = document.createElement('span')
-        span1.className = 'text'
-        span1.textContent = rows[j]
-        kbd1.className = 'kbd1'
+        kbd1 = tag('kbd', {className: 'kbd1'})
+        span1 = tag('span', {className: 'text', textContent: rows[j]})
         kbd1.appendChild(span1)
 
         /*显示网站图标*/
-        img1 = document.createElement('img')
-        img1.className = 'icon'
+        img1 = tag('img', {className: 'icon'})
         if(hash[rows[j]]){
             img1.src = 'http://' + hash[rows[j]] + '/favicon.ico'
         }else{
@@ -50,9 +46,7 @@ for (var i = 0; i < keys.length; i++) {
 
         kbd1.appendChild(img1)
         /*添加编辑按键*/
-        butEdit = document.createElement('button')
-        butEdit.textContent = '编辑'
-        butEdit.id = rows[j]
+        butEdit = tag('button', {id: rows[j],textContent: '编辑'})
         kbd1.appendChild(butEdit)
         /*实现编辑功能*/
         butEdit.onclick = function (butEditPress) {
@@ -61,7 +55,6 @@ for (var i = 0; i < keys.length; i++) {
             hash[key.id] = siteInput
 
             key.previousSibling.src = 'http://' + siteInput + '/favicon.ico'
-            console.log (key.previousSibling)
 
             /*将用户输入的网站保存的浏览器中*/
             var hashInLocal = JSON.stringify(hash)
@@ -76,8 +69,18 @@ for (var i = 0; i < keys.length; i++) {
 
 /*监听用户按下按键然后打开对应的网站*/
 document.onkeypress = function (kbdPress) {
-    key = kbdPress.key
+    key = kbdPress.key.toLowerCase()
+
     /*只打开保存了的网站*/
     if(website = hash[key])
         window.open('http://'+website, '_blank')
+}
+
+function tag(tagName, attribute) {
+    var element = document.createElement(tagName)
+    for (var key in attribute){
+        element[key] = attribute[key]
+    }
+
+    return element
 }
