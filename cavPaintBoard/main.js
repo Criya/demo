3ce54911eb164048abe1eb5c121d9e48a2eb4d6e
ljh <criya713@163.com>
2018-04-17 22:09:14 +0800

    添加了viewport，防止手机自动缩放

--- cavPaintBoard/main.js
+++ cavPaintBoard/main.js
@@ -1,19 +1,79 @@
 var cav = document.getElementById("cav")
 
-//自动设置画板尺寸
-autoSetSize()
 
+autoSetSize()
+var eraserEnable = false
 if (cav) {
+    //canvas初始化
     ctx = cav.getContext("2d")
+    //鼠标是否按下
     var using = false
     var beginPoint = {}
+
     listenMouse(cav)
+    listenAction()
+}
+
+//监听鼠标点击移动
+function listenMouse(cav) {
+    cav.onmousedown = function (omd) {
+        using = true
+        beginPoint = {x: omd.clientX, y: omd.clientY}
+        if (eraserEnable) {
+
+            ctx.clearRect(beginPoint.x - 3, beginPoint.y - 3, 6, 6)
+        }
+    }
+
+    cav.onmousemove = function (omm) {
+        var latestPoint = {x: omm.clientX, y: omm.clientY}
+        if (!using) {
+            return
+        }
+        if (eraserEnable) {
+
+            ctx.clearRect(latestPoint.x - 5, latestPoint.y - 5, 10, 10)
+
+        }
+        else {
+            ctx.beginPath()
+            ctx.lineWidth = 4
+            ctx.moveTo(beginPoint.x, beginPoint.y)
+            ctx.lineTo(latestPoint.x, latestPoint.y)
+            ctx.stroke()
+            beginPoint = latestPoint
+
+        }
+
+
+
+    }
+    cav.onmouseleave = function () {
+        using = false
+    }
+
+    cav.onmouseup = function () {
+        using = false
+    }
+}
+
+//监听左下角橡皮檫和画笔按钮点击
+function listenAction() {
+    eraser.onclick = function () {
+        action.className = "actEra"
+        eraserEnable = true
+    }
+
+    paint.onclick = function () {
+        action.className = "actPaint"
+        eraserEnable = false
+    }
 
 
 }
 
-
-function autoSetSize(){
+//自动设置画板尺寸
+function autoSetSize() {
     resizeBoard()
     window.onresize = function () {
         resizeBoard()
@@ -25,53 +85,3 @@ function resizeBoard() {
     cav.width = document.documentElement.clientWidth
     cav.height = document.documentElement.clientHeight
 }
-
-function listenMouse(cav){
-    cav.onmousedown = function (omd) {
-        beginPoint = {x:omd.clientX,y:omd.clientY}
-        if(eraserEnable){
-            using = true
-            ctx.clearRect(beginPoint.x-3, beginPoint.y-3, 6, 6)
-        }
-        else{
-            using = true
-        }
-
-    }
-
-    cav.onmousemove = function (omm) {
-        var latestPoint = {x:omm.clientX,y:omm.clientY}
-        if (eraserEnable){
-            if(using){
-                ctx.clearRect(latestPoint.x-3, latestPoint.y-3, 6, 6)
-            }
-        }
-        else {
-            if (using) {
-                //
-
-                ctx.beginPath()
-                ctx.lineWidth = 4
-                ctx.moveTo(beginPoint.x, beginPoint.y)
-                ctx.lineTo(latestPoint.x, latestPoint.y)
-                ctx.stroke()
-                beginPoint = latestPoint
-            }
-        }
-
-        cav.onmouseleave = function () {
-            using = false
-        }
-
-    }
-
-    cav.onmouseup = function () {
-        using = false
-    }
-}
-
-var eraserEnable = false
-document.getElementById("eraser").onclick = function () {
-    eraserEnable = !eraserEnable
-}
-
