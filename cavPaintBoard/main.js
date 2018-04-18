a695f7369648889f2e1541c9921daa0ee5e10049
ljh <criya713@163.com>
2018-04-18 08:57:07 +0800

    添加特性检测，支持触摸屏设备

--- cavPaintBoard/main.js
+++ cavPaintBoard/main.js
@@ -10,52 +10,56 @@ if (cav) {
     var using = false
     var beginPoint = {}
 
-    listenMouse(cav)
+    listenUser(cav)
     listenAction()
 }
 
-//监听鼠标点击移动
-function listenMouse(cav) {
-    cav.onmousedown = function (omd) {
-        using = true
-        beginPoint = {x: omd.clientX, y: omd.clientY}
-        if (eraserEnable) {
+//特性检测
+function listenUser(cav) {
+    //触摸屏设备判断
+    if ('ontouchstart' in document.body) {
+        cav.ontouchstart = function (ots) {
+            using = true
+            beginPoint = {x: ots.touches[0].clientX, y: ots.touches[0].clientY}
+            if (eraserEnable) {
+                ctx.clearRect(beginPoint.x - 3, beginPoint.y - 3, 6, 6)
+            }
+        }
 
-            ctx.clearRect(beginPoint.x - 3, beginPoint.y - 3, 6, 6)
+        cav.ontouchmove = function (otm) {
+            var latestPoint = {x: otm.touches[0].clientX, y: otm.touches[0].clientY}
+            draw(latestPoint)
+        }
+
+        cav.ontouchend = function () {
+            using = false
+        }
+    }
+    //非触摸屏设备
+    else {
+        cav.onmousedown = function (omd) {
+            using = true
+            beginPoint = {x: omd.clientX, y: omd.clientY}
+            if (eraserEnable) {
+                ctx.clearRect(beginPoint.x - 3, beginPoint.y - 3, 6, 6)
+            }
+        }
+
+        cav.onmousemove = function (omm) {
+            var latestPoint = {x: omm.clientX, y: omm.clientY}
+            draw(latestPoint)
+        }
+        cav.onmouseleave = function () {
+            using = false
+        }
+
+        cav.onmouseup = function () {
+            using = false
         }
     }
 
-    cav.onmousemove = function (omm) {
-        var latestPoint = {x: omm.clientX, y: omm.clientY}
-        if (!using) {
-            return
-        }
-        if (eraserEnable) {
-
-            ctx.clearRect(latestPoint.x - 5, latestPoint.y - 5, 10, 10)
-
-        }
-        else {
-            ctx.beginPath()
-            ctx.lineWidth = 4
-            ctx.moveTo(beginPoint.x, beginPoint.y)
-            ctx.lineTo(latestPoint.x, latestPoint.y)
-            ctx.stroke()
-            beginPoint = latestPoint
-
-        }
-
-
-
-    }
-    cav.onmouseleave = function () {
-        using = false
-    }
-
-    cav.onmouseup = function () {
-        using = false
-    }
 }
+
 
 //监听左下角橡皮檫和画笔按钮点击
 function listenAction() {
@@ -70,6 +74,26 @@ function listenAction() {
     }
 
 
+}
+
+//画
+function draw(latestPoint) {
+    if (!using) {
+        return
+    }
+    if (eraserEnable) {
+
+        ctx.clearRect(latestPoint.x - 5, latestPoint.y - 5, 10, 10)
+
+    }
+    else {
+        ctx.beginPath()
+        ctx.lineWidth = 4
+        ctx.moveTo(beginPoint.x, beginPoint.y)
+        ctx.lineTo(latestPoint.x, latestPoint.y)
+        ctx.stroke()
+        beginPoint = latestPoint
+    }
 }
 
 //自动设置画板尺寸
